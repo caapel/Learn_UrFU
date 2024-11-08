682ad3a3d1ee0c4c3601823c0856e8a434f183a8
caapel <caapel@mail.ru>
2024-11-08 22:44:48 +0300

    solve conflict testLearning

---
+++ Learn_UrFU/testLearning.cs
@@ -0,0 +1,26 @@
+﻿/*namespace Learn_UrFU
+{
+    internal class Trigonometria
+    {
+        public static void Sin_Cos(double Angle, out double SinAngle, out double CosAngle)
+        {
+            SinAngle = Math.Sin(Angle);
+            CosAngle = Math.Cos(Angle);
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double sin, cos;
+
+            Trigonometria.Sin_Cos(Math.PI / 4, out sin, out cos);
+
+            Console.WriteLine(string.Format("Sin={0} Cos={1}", sin, cos));
+        }
+    
+    
+    }
+
+}*/
