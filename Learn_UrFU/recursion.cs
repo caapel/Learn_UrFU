9cb5022e931a5d53713f528780c0609b2d6b9f6e
caapel <caapel@mail.ru>
2024-11-08 23:03:16 +0300

    recursion

---
+++ Learn_UrFU/recursion.cs
@@ -0,0 +1,19 @@
+﻿namespace Learn_UrFU
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            WriteReversed(new char[] { '1', '2', '3', '4', '5'});
+        }
+        
+        public static void WriteReversed(char[] items, int startIndex = 0)
+        {
+            if (startIndex < items.Length)
+            {
+                WriteReversed(items, startIndex + 1);
+                Console.Write(items[startIndex]);
+            }
+        }
+    }
+}
