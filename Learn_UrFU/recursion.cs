eb60e3387a155734c1afdc541bea7e14344a683e
caapel <caapel@mail.ru>
2024-11-29 11:50:54 +0300

    recursion

--- Learn_UrFU/recursion.cs
+++ Learn_UrFU/recursion.cs
@@ -4,15 +4,47 @@
     {
         public static void Main()
         {
-            WriteReversed(new char[] { '1', '2', '3', '4', '5'});
+            //TestOnSize(1);
+            TestOnSize(2);
+            //TestOnSize(0);
+            //TestOnSize(3);
+            //TestOnSize(4);
         }
-        
-        public static void WriteReversed(char[] items, int startIndex = 0)
+
+        static void TestOnSize(int size)
         {
-            if (startIndex < items.Length)
+            var result = new List<int[]>();
+            MakePermutations(new int[size], 0, result);
+            foreach (var permutation in result) // вызываем на печать комбинации в result
+                WritePermutation(permutation);
+        }
+
+        static void WritePermutation(int[] permutation)
+        {
+            var strings = new List<string>();
+            foreach (var i in permutation)
+                strings.Add(i.ToString());
+            Console.WriteLine(string.Join(" ", strings.ToArray()));
+        }
+
+        static void MakePermutations(int[] permutation, int position, List<int[]> result)
+        {
+            if (position == permutation.Length)
             {
-                WriteReversed(items, startIndex + 1);
-                Console.Write(items[startIndex]);
+                result.Add(permutation.ToArray());
+            }
+            else
+            {
+                for (int i = 0; i < permutation.Length; i++)
+                {
+                    var index = Array.IndexOf(permutation, i, 0, position);
+
+                    if (index == -1)
+                    { 
+                        permutation[position] = i;
+                        MakePermutations(permutation, position + 1, result);
+                    }           
+                }
             }
         }
     }
